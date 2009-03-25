a6717c39783a150f978a2c7a02b2c953252be681
kiwanami <kiwanami@e0ada529-2ee4-dc11-9ed5-00123f7609e0>
2009-03-25 05:25:27 +0000

    bug fixed and added license
    git-svn-id: http://svn.codecheck.in/platform/screenlets@1263 e0ada529-2ee4-dc11-9ed5-00123f7609e0

--- JPWeatherRadar/JPWeatherRadarScreenlet.py
+++ JPWeatherRadar/JPWeatherRadarScreenlet.py
@@ -5,6 +5,18 @@
 # INFO:
 # - Show the weather radar images.
 # 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
 from __future__ import with_statement
@@ -82,9 +94,8 @@ class JPWeatherRadarScreenlet (screenlets.Screenlet):
         def on_after_set_atribute(self,name, value):
 		print name + ' is going to change from ' + str(value)
 		if name == "local_code":
-			if value != '' and value != self.local_code:
-                                print "Setting local_code for JPWeatherRadarScreenlet: %s" % value
-                                self.update_image()
+                        print "Setting local_code for JPWeatherRadarScreenlet: %s" % value
+                        self.update_image()
                 elif name == "update_interval":
 			if value <= 0:
                                 value = 1
